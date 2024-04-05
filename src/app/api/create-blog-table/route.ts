c7b4a17c48de82369b5cc5217b25f7fe8c97d727
minjae07206 <minjaekor@gmail.com>
2024-04-05 23:24:06 +0400

    creating blog database

---
+++ src/app/api/create-blog-table/route.ts
@@ -0,0 +1,19 @@
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
+ 
+export async function GET(request: Request) {
+  try {
+    const result =
+      await sql`CREATE TABLE IF NOT EXISTS blog ( 
+        post_uuid UUID NOT NULL PRIMARY KEY,
+        title VARCHAR(50) NOT NULL,
+        description VARCHAR(100) NOT NULL,
+        date_of_last_modify DATE NOT NULL,
+        cover_image VARCHAR(100) NOT NULL,
+        category VARCHAR(50) NOT NULL,
+        content TEXT NOT NULL;`;
+    return NextResponse.json({ result }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
