7e9d514b6567daec72263d59a8eba762bf855d2e
minjae07206 <minjaekor@gmail.com>
2024-04-14 22:16:39 +0400

    added comments to error page

--- src/app/Tech/[id]/error.tsx
+++ src/app/Tech/[id]/error.tsx
@@ -1,5 +1,9 @@
 "use client"
 {/* error page has to be a client component because there are ways to recover from an error */}
+
+{/* The file name matters, not the function name */}
+{/* If we don't use the error page, the whole application will fail */}
+{/* Only the page  inside [id] folder will fail, so the nav bar will be rendered. */}
 export default function Error() {
     return (
         <div style={{ textAlign: 'center', fontSize: '100px' }}>
