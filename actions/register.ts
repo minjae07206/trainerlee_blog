744a4637dc88918e13d8bb2d6a612053262d9c1f
minjae07206 <minjaekor@gmail.com>
2024-04-23 18:19:14 +0400

    created prisma database for user

--- actions/register.ts
+++ actions/register.ts
@@ -1,5 +1,26 @@
 'use server'
-
-export const register = (name: string, email: string, password: string) => {
+import bycrypt from "bcrypt";
+import { db } from "@/lib/db"
+import { error } from "console";
+import { getUserByEmail } from "../data/user";
+export const register = async (name: string, email: string, password: string) => {
     console.log(name, email, password)
+    // hash password using bcrypt, 10 is the salt
+    const hashedPassword = await bycrypt.hash(password, 10)
+    const existingUser = await getUserByEmail(email);
+    if (existingUser) {
+        return { error: "Email already in use!" } 
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword,
+        }
+    })
+
+    // Todo: Send verification email
+
+    return { success: 'User created!' }
 }
