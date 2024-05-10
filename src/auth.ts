e87055cd7c4ce0609e4348e24a0b7b6612cf7cbd
minjae07206 <minjaekor@gmail.com>
2024-05-10 09:12:21 +0400

    adding user role

--- src/auth.ts
+++ src/auth.ts
@@ -3,6 +3,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 import authConfig from "./auth.config";
 import { db } from '@/app/lib/db';
+import { getUserById } from "./data/user";
   
 export const { 
   handlers, 
@@ -11,8 +12,23 @@ export const {
   signOut,
  } = NextAuth({
   callbacks: {
+    async session ({ token, session }) {
+      console.log({sessionToken: token});
+      if ( token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      console.log({sessionToken: token});
+      return session;
+    },
     async jwt ({ token }) {
       console.log(token);
+      // !token.sub means currently logged out. token.sub is the id in database.
+      if (!token.sub) return token;
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+      token.role = existingUser.role
+
       return token;
     }
   },
