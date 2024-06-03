1fab5ed0cc4c31c4c3cecb877d50d22825b219c0
minjae07206 <minjaekor@gmail.com>
2024-06-03 22:53:43 +0900

    added new file: understanding promise

--- src/components/resume_navigation.tsx
+++ src/components/resume_navigation.tsx
@@ -43,11 +43,12 @@ export const ResumeNavigation = () => {
                                 <h4 className={styles.categoryTag}>{categoryTag.children[0].innerHTML}</h4>
                                 <ul>
                                 {
-                                    // Within the categories, extract each school, job, project to get names of each one.
+                                    
                                     Array.from(categoryTag.children[2].children).map((activityTag, id)=>{
+                                        // This handleResumeNavClick function scrolls to the section of the resume when the 
+                                        // user clicks on a certain tag on the navigation on the left side of the screen.
                                         const handleResumeNavClick = (event: React.MouseEvent<HTMLElement, MouseEvent>, liId: string) => {
                                             event.preventDefault();
-                                            // Using querySelector doesn't work? Why?
                                             const resumeContent = Array.from(document.querySelectorAll('.activity-section'));
                                             for (let activity of resumeContent) {
                                                 if (activity.children[1].innerHTML === liId) {
@@ -57,7 +58,8 @@ export const ResumeNavigation = () => {
                                             
                                           };
                                         return (
-                                            
+                                                // Maybe I could change the structure so the code is more readable, children[1] can be confusing
+                                                // Within the categories, extract each school, job, project to get names of each one.
                                                 <li style={{display: 'block'}} onClick={(event)=>{handleResumeNavClick(event, activityTag.children[1].innerHTML)}} id={activityTag.children[1].innerHTML} className={`${styles.activityTag} ${activeText === activityTag.children[1].innerHTML ? styles.active : ''}`} key={id}>
                                                     • {activityTag.children[1].innerHTML}
                                                 </li>
